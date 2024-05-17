fbcdca8af20558785acc056f6d6abbfa02c5ea02
Pedro Gomez <augustopgomez1994@gmail.com>
2024-05-17 17:40:29 -0300

    Clase Aula

--- src/Aula/Aula.java
+++ src/Aula/Aula.java
@@ -1,5 +1,89 @@
 package Aula;
 
-public class Aula {
-    private int id;
+import java.util.Objects;
+
+public abstract class Aula {
+    private int numeroAula;
+    private int capacidad;
+    private boolean proyector;
+    private boolean tele;
+    private boolean disponible;
+
+
+    public Aula(int numeroAula, int capacidad, boolean proyector, boolean tele, boolean disponible) {
+        this.numeroAula = numeroAula;
+        this.capacidad = capacidad;
+        this.proyector = proyector;
+        this.tele = tele;
+        this.disponible = disponible;
+    }
+
+    public int getNumeroAula() {
+        return numeroAula;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public boolean isProyector() {
+        return proyector;
+    }
+
+    public void setProyector(boolean proyector) {
+        this.proyector = proyector;
+    }
+
+    public boolean isTele() {
+        return tele;
+    }
+
+    public void setTele(boolean tele) {
+        this.tele = tele;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    @Override
+    public String toString() {
+        return "Aula{" +
+                "numeroAula=" + numeroAula +
+                ", capacidad=" + capacidad +
+                ", proyector=" + proyector +
+                ", tele=" + tele +
+                ", disponible=" + disponible +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        boolean esIgual=false;
+        if(o!=null)
+        {
+            if(o instanceof Aula)
+            {
+                Aula aula=(Aula)o;
+                if(aula.numeroAula == getNumeroAula())
+                {
+                    esIgual=true;
+                }
+            }
+        }
+        return esIgual;
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
+    }
 }
---
+++ src/Aula/AulaComputadora.java
@@ -0,0 +1,35 @@
+package Aula;
+
+public class AulaComputadora extends Aula{
+    private int cantidadComputadoras;
+    private boolean auriculares;
+
+    public AulaComputadora(int numeroAula, int capacidad, boolean proyector, boolean tele, boolean disponible, int cantidadComputadoras, boolean auriculares) {
+        super(numeroAula, capacidad, proyector, tele, disponible);
+        this.cantidadComputadoras = cantidadComputadoras;
+        this.auriculares = auriculares;
+    }
+
+    public int getCantidadComputadoras() {
+        return cantidadComputadoras;
+    }
+
+    public void setCantidadComputadoras(int cantidadComputadoras) {
+        this.cantidadComputadoras = cantidadComputadoras;
+    }
+
+    public boolean isAuriculares() {
+        return auriculares;
+    }
+
+    public void setAuriculares(boolean auriculares) {
+        this.auriculares = auriculares;
+    }
+
+    @Override
+    public String toString() {
+        return "AulaComputadora{" +
+                "cantidadComputadoras=" + cantidadComputadoras +
+                ", auriculares=" + auriculares + super.toString();
+    }
+}
