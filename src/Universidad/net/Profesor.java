3cef837edc0aeafb7934e71ffe5a8ff1395c40e2
Lopez Sebastian <sebastian.lopez.mdq@gmail.com>
2024-05-17 17:30:53 -0300

    Cree clase profesor

---
+++ src/Universidad/net/Profesor.java
@@ -0,0 +1,37 @@
+package Universidad.net;
+
+public class Profesor {
+    private String nombre;
+    private String apellido;
+    private int legajo;
+
+    public Profesor(String nombre,String apellido, int legajo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.legajo = legajo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+}
