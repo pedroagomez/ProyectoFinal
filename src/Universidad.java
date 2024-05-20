2ade63c95440a586e421c26099dae7f8a5beab7d
Lopez Sebastian <sebastian.lopez.mdq@gmail.com>
2024-05-20 15:19:22 -0300

    Funcion de busqueda y creacion de profesor

--- src/Universidad.java
+++ src/Universidad.java
@@ -2,6 +2,7 @@ import GestorColeccion.GestorColeccion;
 import Reserva.Reserva;
 import Universidad.net.*;
 
+import javax.swing.*;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -19,7 +20,7 @@ public class Universidad {
         this.reservas = new LinkedList<>();;
     }
 
-    public void cargarProfesores() {
+    public void cargarProfesoresAutomatico() {
         profesores.add(new Profesor("Rodrigo","Albert",1 ));
         profesores.add(new Profesor("Paula","Fernandez",2 ));
         profesores.add(new Profesor("Costanza","Nandez",3 ));
@@ -29,5 +30,25 @@ public class Universidad {
         profesores.add(new Profesor("Ani","Skaywito",7 ));
         profesores.add(new Profesor("Paulo","Mendez",8 ));
         profesores.add(new Profesor("Emanuel","Lopez",9 ));
+    }
+
+    public boolean cargarProfesorNuevo(String nombre,String apellido, int Legajo) {
+        boolean correcto = false;
+        Profesor aux = new Profesor(nombre,apellido,Legajo);
+        if (!profesores.contains(aux)) {
+            profesores.add(new Profesor(nombre,apellido,Legajo));
+            correcto = true;
+        }
+        return correcto;
+    }
+
+    public Profesor buscarYdevolverProfesorPorLegajo(int legajo){
+        Profesor aux = null;
+        for(Profesor profesor : profesores){
+            if(profesor.getLegajo() == legajo){
+                aux = profesor;
+            }
+        }
+        return aux;
     }
 }
