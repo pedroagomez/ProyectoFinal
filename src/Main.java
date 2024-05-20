32b2b85bbee0ea665733e7e1404ec04be321b251
Lopez Sebastian <sebastian.lopez.mdq@gmail.com>
2024-05-20 15:04:53 -0300

    Cambios varios

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String [] args)
     {
-        System.out.println("Hola mundo probando la nueva version de intll sin fallos PEDRO PEDRO PEDRO PEP EP EPEPE");
+        Universidad miUniversidad = new Universidad();
+
     }
 }
--- src/Universidad.java
+++ src/Universidad.java
@@ -1,9 +1,23 @@
 import GestorColeccion.GestorColeccion;
+import Reserva.Reserva;
+import Universidad.net.*
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 
 public class Universidad {
+    private HashSet<Profesor> profesores;
+    LinkedList<Materia> materias;
+    LinkedList<Reserva> reservas
+    ///HashMap<K,V> miMapa = new HashMap<K, V>();
 
-   
 
+    public Universidad() {
+        this.profesores = new HashSet <>();
+        this.materias = new LinkedList<>();
+        this.reservas = new LinkedList<>();;
+    }
 
 
 }
