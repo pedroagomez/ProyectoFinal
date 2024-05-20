d09efe764019392989395f634b56683ae7f33707
Lopez Sebastian <sebastian.lopez.mdq@gmail.com>
2024-05-20 15:10:45 -0300

    Funcion Carga Profesores

--- src/Universidad.java
+++ src/Universidad.java
@@ -1,6 +1,6 @@
 import GestorColeccion.GestorColeccion;
 import Reserva.Reserva;
-import Universidad.net.*
+import Universidad.net.*;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -9,7 +9,7 @@ import java.util.LinkedList;
 public class Universidad {
     private HashSet<Profesor> profesores;
     LinkedList<Materia> materias;
-    LinkedList<Reserva> reservas
+    LinkedList<Reserva> reservas;
     ///HashMap<K,V> miMapa = new HashMap<K, V>();
 
 
@@ -19,5 +19,15 @@ public class Universidad {
         this.reservas = new LinkedList<>();;
     }
 
-
+    public void cargarProfesores() {
+        profesores.add(new Profesor("Rodrigo","Albert",1 ));
+        profesores.add(new Profesor("Paula","Fernandez",2 ));
+        profesores.add(new Profesor("Costanza","Nandez",3 ));
+        profesores.add(new Profesor("Maximiliano","Expert",4 ));
+        profesores.add(new Profesor("Dexster","Morgan",5 ));
+        profesores.add(new Profesor("Ben","Kenobi",6 ));
+        profesores.add(new Profesor("Ani","Skaywito",7 ));
+        profesores.add(new Profesor("Paulo","Mendez",8 ));
+        profesores.add(new Profesor("Emanuel","Lopez",9 ));
+    }
 }
